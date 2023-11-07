66642e99ab5242511a55c3c6815fac1cce44fd2f
ssakibs04 <sakibshadman34@gmail.com>
2023-11-07 20:15:07 +0600

    First Commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using inventory_ManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace inventory_ManagementSystem.Controllers
+{
+    public class ProductController : Controller
+    {
+		Inventory_managementEntities db = new Inventory_managementEntities();
+		// GET: Product
+		public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DisplayProduct()
+        {
+            List<Product> list = db.Products.OrderByDescending(x=>x.id).ToList();
+            return View(list);    
+        }
+        [HttpGet]
+        public ActionResult CreateProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateProduct(Product pro)
+        {
+            db.Products.Add(pro);
+            db.SaveChanges();
+            return RedirectToAction("DisplayProduct");
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="inventory_ManagementSystem.MvcApplication" Language="C#" %>
